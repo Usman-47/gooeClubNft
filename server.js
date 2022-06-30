f45557c6d2af6065d905b45b688172d53755169a
Usman-47 <usmanjamil047@gmail.com>
2022-06-30 21:33:46 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+
+app.use(cors());
+app.use(express.static("./build"));
+app.all("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log("Server listening on port 8000!");
+});
